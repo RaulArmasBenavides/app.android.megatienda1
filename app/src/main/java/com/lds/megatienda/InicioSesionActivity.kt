4b99265269f3aa36ee6dd544ba6cba56f3995553
Raúl Armas <raularmasbx@gmail.com>
2022-08-25 03:16:07 -0500

    updates

---
+++ app/src/main/java/com/lds/megatienda/InicioSesionActivity.kt
@@ -0,0 +1,12 @@
+package com.lds.megatienda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioSesionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion)
+    }
+}
